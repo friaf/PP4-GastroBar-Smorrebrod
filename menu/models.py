0da4766af35d716206f7296ef91e1c9e30a0182e
friaf <shovel.irena@gmail.com>
2023-05-03 17:51:48 +0000

    Add menu models and admin panel

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class BestItem(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    price = models.DecimalField(max_digits=4, decimal_places=2, blank=False, default=False)
+    image = CloudinaryField('image', blank=False, default=False)
+
+    def __str__(self):
+        return str(self.name) + " " + str(self.price)
